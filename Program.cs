208c88e91a5d38450788fe53f63d47d16eedabce
Mike331122 <micpel60@wp.pl>
2024-05-27 19:39:35 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Prosty Kalkulator");
+            Console.WriteLine("Co chciciałbyś zrobić ?");
+
+            Console.ReadKey();
+        }
+    }
+}
